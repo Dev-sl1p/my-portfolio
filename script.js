f5d8427fc2ae23ea97b7d839ebff864d22aed64b
Dev-sl1p <77191510+Dev-sl1p@users.noreply.github.com>
2025-03-17 20:05:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    alert("My Portfolio")
+}
+
+function changeText() {
+    document.getElementById("message").innerHTML= "ข้อความถูกเปลี่ยนแล้ว";
+}
+
+function Background() {
+    document.body.style.backgroundColor = "DarkGray";
+}
